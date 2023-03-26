a512fa3088165997355257451a572ea65139fa35
nimo <mengmuan2013@gmail.com>
2023-03-26 23:46:19 +0800

    chore: initial

---
+++ src/config/config.default.ts
@@ -0,0 +1,9 @@
+import { MidwayConfig } from '@midwayjs/core';
+
+export default {
+  // use for cookie sign key, should change to your own and keep security
+  keys: '1679843677111_3756',
+  koa: {
+    port: 7001,
+  },
+} as MidwayConfig;
