879def305da26e0d90796a44cc19a1be55e88c04
nimo <mengmuan2013@gmail.com>
2023-04-07 00:51:54 +0800

    feat: 新增 puppeteer

---
+++ src/util/puppeteer.ts
@@ -0,0 +1,43 @@
+import puppeteer, { Browser } from 'puppeteer';
+
+export async function openPage(doTask: (browser: Browser) => Promise<void>) {
+  const browser = await puppeteer.launch();
+
+  let err: any;
+  await doTask(browser).catch(e => (err = e));
+
+  await browser.close();
+
+  if (err) {
+    return Promise.reject(err);
+  }
+}
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+  await page.goto('https://developer.chrome.com/');
+
+  // Set screen size
+  await page.setViewport({ width: 1080, height: 1024 });
+
+  // Type into search box
+  await page.type('.search-box__input', 'automate beyond recorder');
+
+  // Wait and click on first result
+  const searchResultSelector = '.search-box__link';
+  await page.waitForSelector(searchResultSelector);
+  await page.click(searchResultSelector);
+
+  // Locate the full title with a unique string
+  const textSelector = await page.waitForSelector(
+    'text/Customize and automate'
+  );
+  const fullTitle = await textSelector.evaluate(el => el.textContent);
+
+  // Print the full title
+  console.log('The title of this blog post is "%s".', fullTitle);
+
+  await browser.close();
+})();
